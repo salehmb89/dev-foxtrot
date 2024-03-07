f1e6840f1549d8fc4cccd75ce604cea09aa5d4c3
Saleh Basalim <156353463+salehmb89@users.noreply.github.com>
2024-03-07 14:32:52 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -18,23 +18,22 @@ $(document).ready(function() {
         // Education
         html += '<h3>Education:</h3>';
         for (var i = 0; i < data.education.length; i++) {
-        html += '<div class="resume-section">';
-        html += '<h4>' + data.education[i].degree + '</h4>';
-        html += '<p>' + data.education[i].university + '</p>';
-        html += '<p>' + data.education[i].year + '</p>';
-        html += '</div>';
+            html += '<div class="resume-section">';
+            html += '<h4>' + data.education[i].degree + '</h4>';
+            html += '<p>' + data.education[i].university + '</p>';
+            html += '<p>' + data.education[i].year + '</p>';
+            html += '</div>';
         }
         html += '<hr>';
 
         // Work Experience
         html += '<h3>Work Experience:</h3>';
         for (var j = 0; j < data.experience.length; j++) {
-        html += '<div class="resume-section">';
-        html += '<h4>' + data.experience[j].position + '</h4>';
-        html += '<p>' + data.experience[j].company + '</p>';
-        html += '<p>' + data.experience[j].duration + '</p>';
-       
-        html += '</div>';
+            html += '<div class="resume-section">';
+            html += '<h4>' + data.experience[j].position + '</h4>';
+            html += '<p>' + data.experience[j].company + '</p>';
+            html += '<p>' + data.experience[j].duration + '</p>';
+            html += '</div>';
         }
         html += '<hr>';
 
@@ -42,10 +41,9 @@ $(document).ready(function() {
         html += '<h3>Skills:</h3>';
         html += '<ul>';
         for (var k = 0; k < data.skills.length; k++) {
-        html += '<li>' + data.skills[k] + '</li>';
+            html += '<li>' + data.skills[k] + '</li>';
         }
-        html += '</ulr>';
-
+        html += '</ul>';  // Fixed the typo here
 
         resumeData.html(html);
     });
